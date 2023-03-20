7f194ef4fe461d486f96c46147cb4972391b441b
KarolSalazar <Karolv.salazarq@uqvirtual.edu.co>
2023-03-19 22:57:04 -0500

    Creado el punto 1 del parcial

--- Punto1.py
+++ Punto1.py
@@ -2,7 +2,29 @@
 #       x
 
 import math
+import random
+
+class Punto1:
+    def funcion(x):
+        f = math.sin(x)/x
+        return f
+    
+    def generar_random():
+        x = random.randint(-999,999)
+        if x==0:
+            return Punto1.generar_random()
+        return x
+    
+    def ejecutar():
+        x = Punto1.generar_random()
+        print("Valor de X: " + str(x))
+        f = Punto1.funcion(x)
+        print("Valor obtenido en la función: " + str(f))
+
+Punto1.ejecutar()
+
 
 """
-Aquí se supone que debemos poner lo que va interamente dentro del punto 1, y sólo definí la función :D
+Se importan math y random, se crea un método función el cuál retorna una función que dependa de x,
+se crea una función que genera aleatoriamente un valor de x, se crea la función de ejecutar
 """
